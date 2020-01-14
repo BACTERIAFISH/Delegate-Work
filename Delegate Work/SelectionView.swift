434f455a846b83bcd7e22a4c9b782e5fc3908495
Bacteriafish <you@example.com>
2020-01-14 09:56:56 +0800

    first commit complete delegate work

---
+++ Delegate Work/SelectionView.swift
@@ -0,0 +1,141 @@
+//
+//  SelectionView.swift
+//  Delegate Work
+//
+//  Created by FISH on 2020/1/13.
+//  Copyright © 2020 FISH. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol SelectionViewDataSource: AnyObject {
+    
+    func numberOfSelections(_ selectionView: SelectionView) -> Int
+    
+    func selectionView(_ selectionView: SelectionView, titleForSelection selection: Int) -> String?
+    
+    func selectionView(_ selectionView: SelectionView, colorForSelectionTitle selection: Int) -> UIColor
+    
+    func selectionView(_ selectionView: SelectionView, fontForSelectionTitle selection: Int) -> UIFont
+    
+    func colorForUnderline(_ selectionView: SelectionView) -> UIColor
+}
+
+protocol SelectionViewDelegate: AnyObject {
+    
+    func selectionView(_ selectionView: SelectionView, didSelect selection: Int)
+    
+    func selectionView(_ selectionView: SelectionView, disable selection: Int) -> Bool
+}
+
+extension SelectionViewDelegate {
+    
+    func selectionView(_ selectionView: SelectionView, didSelect selection: Int) {}
+    
+    func selectionView(_ selectionView: SelectionView, disable selection: Int) -> Bool {
+        return false
+    }
+}
+
+class SelectionView: UIView {
+    
+    var dataSource: SelectionViewDataSource? {
+        didSet {
+            setButtons()
+            setUnderline()
+            setConstraints()
+        }
+    }
+    
+    var delegate: SelectionViewDelegate?
+    
+    private let stackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.alignment = .fill
+        stack.distribution = .fillEqually
+        stack.spacing = 0
+        return stack
+    }()
+        
+    private var buttons = [UIButton]()
+    
+    private let underline = UIView()
+    
+    private var underlineLeadingConstraint: NSLayoutConstraint!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setButtons() {
+        
+        let numbers = dataSource?.numberOfSelections(self) ?? 2
+        
+        for i in 0..<numbers {
+            let button = UIButton()
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.backgroundColor = .gray
+            button.setTitle(dataSource?.selectionView(self, titleForSelection: i) ?? "test", for: .normal)
+            button.setTitleColor(dataSource?.selectionView(self, colorForSelectionTitle: i) ?? UIColor.white, for: .normal)
+            button.titleLabel?.font = dataSource?.selectionView(self, fontForSelectionTitle: i) ?? UIFont.systemFont(ofSize: 18)
+            button.addTarget(self, action: #selector(pressed(sender:)), for: .touchUpInside)
+            stackView.addArrangedSubview(button)
+            buttons.append(button)
+        }
+        
+        self.addSubview(stackView)
+    }
+    
+    private func setUnderline() {
+        underline.translatesAutoresizingMaskIntoConstraints = false
+        underline.backgroundColor = dataSource?.colorForUnderline(self) ?? UIColor.systemBlue
+        self.addSubview(underline)
+    }
+    
+    private func setConstraints() {
+        underlineLeadingConstraint = underline.leadingAnchor.constraint(equalTo: leadingAnchor)
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: underline.topAnchor),
+            underlineLeadingConstraint,
+            underline.bottomAnchor.constraint(equalTo: bottomAnchor),
+            underline.heightAnchor.constraint(equalToConstant: 1),
+            underline.widthAnchor.constraint(equalTo: buttons[0].widthAnchor)
+            
+        ])
+    }
+    
+    @objc private func pressed(sender: UIButton) {
+        let index = buttons.firstIndex(of: sender)!
+        let disable = delegate?.selectionView(self, disable: index) ?? false
+        
+        if !disable {
+            moveUnderline(moveTo: sender.layer.frame.origin.x)
+            delegate?.selectionView(self, didSelect: buttons.firstIndex(of: sender)!)
+        }
+    }
+    
+    private func moveUnderline(moveTo x: CGFloat) {
+        UIViewPropertyAnimator.runningPropertyAnimator(
+            withDuration: 0.5,
+            delay: 0,
+            options: .curveEaseInOut,
+            animations: {
+                self.underlineLeadingConstraint.constant = x
+                self.layoutIfNeeded()
+            },
+            completion: nil
+        )
+    }
+    
+}
---
+++ Delegate Work/ViewController.swift
@@ -0,0 +1,125 @@
+//
+//  ViewController.swift
+//  Delegate Work
+//
+//  Created by FISH on 2020/1/13.
+//  Copyright © 2020 FISH. All rights reserved.
+//
+
+import UIKit
+
+struct SCColor {
+    var title: String
+    var color: UIColor
+}
+
+class ViewController: UIViewController {
+    
+    var selectionView1: SelectionView!
+    var selectionView2: SelectionView!
+    
+    var show1: UIView!
+    var show2: UIView!
+    
+    var selected1 = 0
+    var selected2 = 0
+    
+    var colors1: [SCColor] = [
+        SCColor(title: "red", color: .red),
+        SCColor(title: "blue", color: .blue),
+        SCColor(title: "green", color: .green)
+    ]
+    
+    var colors2: [SCColor] = [
+        SCColor(title: "red", color: .red),
+        SCColor(title: "blue", color: .blue),
+        SCColor(title: "green", color: .green),
+        SCColor(title: "yellow", color: .yellow)
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+        
+        selectionView1 = SelectionView(frame: CGRect(x: 0, y: 50, width: view.frame.width, height: 50))
+        selectionView1.dataSource = self
+        selectionView1.delegate = self
+        view.addSubview(selectionView1)
+        
+        show1 = UIView(frame: CGRect(x: 0, y: 120, width: view.frame.width, height: 100))
+        show1.backgroundColor = colors1[0].color
+        view.addSubview(show1)
+        
+        selectionView2 = SelectionView(frame: CGRect(x: 0, y: 300, width: view.frame.width, height: 50))
+        selectionView2.dataSource = self
+        selectionView2.delegate = self
+        view.addSubview(selectionView2)
+        
+        show2 = UIView(frame: CGRect(x: 0, y: 370, width: view.frame.width, height: 100))
+        show2.backgroundColor = colors1[0].color
+        view.addSubview(show2)
+    }
+    
+}
+
+extension ViewController: SelectionViewDataSource {
+    
+    func numberOfSelections(_ selectionView: SelectionView) -> Int {
+        if selectionView == selectionView1 {
+            return colors1.count
+        } else {
+            return colors2.count
+        }
+    }
+    
+    func selectionView(_ selectionView: SelectionView, titleForSelection selection: Int) -> String? {
+        if selectionView == selectionView1 {
+            return colors1[selection].title
+        } else {
+            return colors2[selection].title
+        }
+    }
+    
+    func selectionView(_ selectionView: SelectionView, colorForSelectionTitle selection: Int) -> UIColor {
+        if selectionView == selectionView1 {
+            return colors1[selection].color
+        } else {
+            return colors2[selection].color
+        }
+        
+    }
+    
+    func selectionView(_ selectionView: SelectionView, fontForSelectionTitle selection: Int) -> UIFont {
+        return UIFont.systemFont(ofSize: 20)
+    }
+    
+    func colorForUnderline(_ selectionView: SelectionView) -> UIColor {
+        return .white
+    }
+
+}
+
+extension ViewController: SelectionViewDelegate {
+    
+    func selectionView(_ selectionView: SelectionView, didSelect selection: Int) {
+        if selectionView == selectionView1 {
+            selected1 = selection
+            show1.backgroundColor = colors1[selection].color
+        } else if selectionView == selectionView2 {
+            selected2 = selection
+            show2.backgroundColor = colors2[selection].color
+        }
+    }
+    
+    func selectionView(_ selectionView: SelectionView, disable selection: Int) -> Bool {
+        if selectionView == selectionView2 {
+            if selected1 == colors1.count - 1 {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+}
